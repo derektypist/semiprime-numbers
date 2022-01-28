8e03e66a6f667767df405968076387976b72edf5
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2022-01-28 11:06:33 +0000

    Added clearInfo function at script.js

--- js/script.js
+++ js/script.js
@@ -43,3 +43,9 @@ function isPrime(n) {
     }
     return true;
 }
+
+// Function to Clear Information
+function clearInfo() {
+    let txt = "";
+    document.getElementById('numinfo').innerHTML = txt;
+}
